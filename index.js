11ab149f54eb9d9225defa101cc89f502c8bbf38
LGocool <67392650+LGocool@users.noreply.github.com>
2022-11-09 23:17:11 -0500

    commit tree e8b44570a61d5b6835f4c0411bbfcbec8c44185e

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+const employee={
+    person:{}, 
+    streetAddress:{} }
+ function updateEmployeeWithKeyAndValue(employee,person, streetAddress){
+    const copyOfEmployee = {...employee};
+        copyOfEmployee[person] = streetAddress;
+    return copyOfEmployee;
+ }
+
+ function destructivelyUpdateEmployeeWithKeyAndValue(employee,person,streetAddress){
+    employee[person]= streetAddress;
+        return employee;
+ }
+
+ function deleteFromEmployeeByKey(employee,person,){
+    const copyOfEmployee = {...employee};
+         delete copyOfEmployee[person];
+             return copyOfEmployee;
+ }
+
+ function  destructivelyDeleteFromEmployeeByKey(employee,streetAddress){
+    delete employee[streetAddress];
+        return employee;
+ }
+
